8c3858271c5b3c72bd9200d10ad3acf36dfff83f
porwalshreyaa <porwalshreya11111@gmail.com>
2024-01-12 18:05:57 +0530

    commit

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -2,7 +2,9 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 // import Col from "react-bootstrap/Col";
-import pic from "./../picture1.jpeg";
+import voiz from "./../voiz.jpeg";
+import portfolio from "./../portfolio.jpeg";
+import sangeet from "./../sangeet.jpeg";
 import Badge from "react-bootstrap/Badge";
 import CardF from './card.js'
 
@@ -14,11 +16,11 @@ function Projects() {
                   style={{ margin: 0.5 + "em"}}>Projects</Badge></h1>
       </Row>
       <Row  className='justify-content-center' >
-        <CardF className={'p1'} title={"Sangeet"} text={"flask music streaming app"} url={"https://github.com/porwalshreyaa/SanGeet"} image={pic}/>
-        <CardF  className={'p2'}  title={"VoiZ"} text={"voice desktop assistant"}  url={"https://freecodecamp.org"} image={pic}/>
-        <CardF className={'p3'} title={"Mad1-Project"} text={"upcoming project"} url={"https://freecodecamp.org"} image={pic}/>
-        <CardF  className={'p4'}  title={"MemoPuzzle"} text={"py-Game"}  url={"https://freecodecamp.org"} image={pic}/>
-        <CardF  className={'p5'}  title={"POrtfolio"} text={"code for this portfolio"}  url={"https://freecodecamp.org"} image={pic}/>
+        <CardF className={'p1'} title={"Sangeet"} text={"flask music streaming app"} url={"https://github.com/porwalshreyaa/SanGeet"} image={sangeet}/>
+        <CardF  className={'p2'}  title={"VoiZ"} text={"voice desktop assistant - in progress"}  url={"https://github.com/porwalshreyaa/Voiz/"} image={voiz}/>
+        {/* <CardF className={'p3'} title={"Mad1-Project"} text={"upcoming project"} url={""} image={pic}/> */}
+        {/* <CardF  className={'p4'}  title={"MemoPuzzle"} text={"py-Game"}  url={""} image={pic}/> */}
+        <CardF  className={'p5'}  title={"Portfolio"} text={"this portfolio"}  url={"https://github.com/porwalshreyaa/NewPortfolio"} image={portfolio}/>
       </Row>
      
       

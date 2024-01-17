00883549dd2618a45b6110a59da594331c92ce26
porwalshreyaa <porwalshreya11111@gmail.com>
2024-01-18 00:04:06 +0530

    update

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -16,11 +16,13 @@ function Projects() {
                   style={{ margin: 0.5 + "em"}}>Projects</Badge></h1>
       </Row>
       <Row  className='justify-content-center' >
-        <CardF className={'p1'} title={"Sangeet"} text={"flask music streaming app"} url={"https://github.com/porwalshreyaa/SanGeet"} image={sangeet}/>
+        <CardF className={'p1'} title={"SanGit"} text={"flask music streaming app"} url={"https://github.com/porwalshreyaa/SanGit"} image={sangeet}/>
         <CardF  className={'p2'}  title={"VoiZ"} text={"voice desktop assistant - in progress"}  url={"https://github.com/porwalshreyaa/Voiz/"} image={voiz}/>
         {/* <CardF className={'p3'} title={"Mad1-Project"} text={"upcoming project"} url={""} image={pic}/> */}
         {/* <CardF  className={'p4'}  title={"MemoPuzzle"} text={"py-Game"}  url={""} image={pic}/> */}
         <CardF  className={'p5'}  title={"Portfolio"} text={"this portfolio"}  url={"https://github.com/porwalshreyaa/NewPortfolio"} image={portfolio}/>
+        <CardF  className={'p6'}  title={"Geeki"} text={"a gadget wiki for students - in progress"}  url={"https://github.com/porwalshreyaa/geeki/"} image={voiz}/>
+        <CardF  className={'p7'}  title={"StudyBuddy"} text={"a study-buddy matchmaker for students - in progress"}  url={"https://github.com/porwalshreyaa/studybuddy/"} image={voiz}/>
       </Row>
      
       
